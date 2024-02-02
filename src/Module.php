f85718b42c0dbb243cd7c8b484bab37565d582f0
provdigi <purnachandra.valluri@gmail.com>
2024-02-02 09:05:51 +0530

    hrthgf

--- src/Module.php
+++ src/Module.php
@@ -8,10 +8,10 @@ class Module extends \luya\forms\Module{
      * {@inheritDoc}
      */
     public function getMenu()
-    { return true;
-        /*return (new \luya\admin\components\AdminMenuBuilder($this))
-            ->node('Forms', 'dynamic_form')
-                ->group('Setup')
+    { 
+        return (new \luya\admin\components\AdminMenuBuilder($this))
+            ->node('Forms', 'dynamic_form');
+            /*    ->group('Setup')
                     ->itemApi('Form', 'forms/form/index', 'dynamic_form', 'api-forms-form')
                 ->group('Data')
                     ->itemApi('Submission', 'forms/submission/index', 'send', 'api-forms-submission')
